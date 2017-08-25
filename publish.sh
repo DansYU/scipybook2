8a719db27646156b53ff613b3acfd1cc5eb252fa
Quanyin Tang <qytang326@gmail.com>
2017-08-25 16:57:57 +0800

    Update Slides

--- publish.sh
+++ publish.sh
@@ -55,6 +55,6 @@ git checkout -B gh-pages
 cp -r /tmp/workspace/* .
 git add -A .
 git commit -m "Update Slides"
-git push origin master gh-pages
+git push -f -u origin master gh-pages
 git checkout master
 rm -rf /tmp/workspace
